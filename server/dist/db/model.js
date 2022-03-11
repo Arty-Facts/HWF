5a99f975ba48be731f59c0d6712dab2718db72b3
rahah039 <rahah039@student.liu.se>
2022-03-11 09:24:36 +0100

    database connect

---
+++ server/dist/db/model.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Task {
+    constructor(msg, id) {
+        this.msg = msg;
+        this.id = id;
+    }
+}
+exports.default = Task;
