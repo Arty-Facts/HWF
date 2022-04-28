ffbe189ec52113285828c41750b93cafda456d67
Somnei <shia@hotmail.se>
2022-04-28 13:15:36 +0200

    Fixed merge conflict

--- daemon/hell.go
+++ daemon/hell.go
@@ -13,6 +13,8 @@ import (
 	message "test.com/test"
 )
 
+var open_files map[string]*os.File
+
 //connect to the server via websockets
 func connect() *websocket.Conn {
 	u := url.URL{
@@ -62,11 +64,10 @@ func listen(connection *websocket.Conn) {
 }
 
 func main() {
+	// init the map for all open output files
+	open_files = make(map[string]*os.File)
 
 	connection := connect()
-
-	builder := flatbuffers.NewBuilder(1024)
-	builder.CreateString("hi")
 
 	// close connection once we go out of scope
 	defer connection.Close()
@@ -96,6 +97,8 @@ func execute_command(command []byte) []byte {
 
 func read_message(msg []byte) {
 
+	fmt.Println("reading message...")
+
 	// right now execute_command is the only
 	// function that can panic, so we know that's
 	// where the error was caused - change this later
@@ -120,108 +123,107 @@ func read_message(msg []byte) {
 		read_file(fb_msg)
 	}
 
-	/*
-		// TASK
-		if msgType == 1 {
-
-			msgTask := test.Task(new(message.Task))
-			msgStage := new(message.Stage)
-			//msgStage :=
-			msgTask.Stages(msgStage, 0)
-
-			var results = make([][]byte, msgStage.CmdListLength())
-			for i := 0; i < msgStage.CmdListLength(); i++ {
-				fmt.Println(string(msgStage.CmdList(i)))
-				result := execute_command(msgStage.CmdList(i))
-				results[i] = result
-			}
-
-			for i, s := range results {
-				fmt.Println(i, s)
-			}
-
-			// msgCmd := msgStage.CmdList(0)
-			//msgStage.CmdList(new(message.CmdList), 0)
-			// fmt.Println(string(msgCmd))
-		}
-
-		// GET_RESULT
-		if msgType == 2 {
-
-		}
-
-		// GET_HARDWAREPOOL
-		if msgType == 3 {
-
-		}
-
-		//FILE
-		if msgType == 4 {
-
-		}
-	*/
-
-	// save all bytes in Data array to arr
-	// for i := 0; i < test.DataLength(); i++ {
-	// 	arr[i] = byte(test.Data(i))
-	// }
-
-	// // save arr to file "hellgo.png"
-	// err := ioutil.WriteFile("hellgo.png", arr, 0644)
-
-	// DEBUG: print the contents of cmd
-	// DEBUG: print the results
-
+	fmt.Println(fb_msg.Type())
 }
 
-// TO-DO: fix this just like read_file
 func read_task(msg *message.Message) {
-	msgTask := msg.Task(new(message.Task))
-	msgStage := new(message.Stage)
 
-	msgTask.Stages(msgStage, 0)
+	fmt.Println("reading task...")
 
-	// TO-DO: Wait before executing the commands!!!!
-	// iterate over cmd and execute all commands
-	var results = make([][]byte, msgStage.CmdListLength())
-	for i := 0; i < msgStage.CmdListLength(); i++ {
-		fmt.Println(string(msgStage.CmdList(i)))
-		result := execute_command(msgStage.CmdList(i))
-		results[i] = result
-	}
+	unionTable := new(flatbuffers.Table)
 
-	// print the results from cmd exec
-	for i, s := range results {
-		fmt.Println(i, s)
+	if msg.Body(unionTable) {
+
+		unionType := msg.BodyType()
+
+		if unionType == message.MessageBodyTask {
+			unionTask := new(message.Task)
+			unionTask.Init(unionTable.Bytes, unionTable.Pos)
+
+			// get all stages from the task
+			stages := make([]*message.Stage, unionTask.StagesLength())
+			tempStage := new(message.Stage)
+
+			for i := 0; i < unionTask.StagesLength(); i++ {
+
+				if unionTask.Stages(tempStage, i) {
+					stages[i] = tempStage
+					var results = make([][]byte, tempStage.CmdListLength())
+					// execute the stage's commands
+					for i := 0; i < tempStage.CmdListLength(); i++ {
+						//fmt.Println(string(tempStage.CmdList(i)))
+						result := execute_command(tempStage.CmdList(i))
+						results[i] = result
+					}
+
+					// print the results from cmd exec
+
+					for i, s := range results {
+						fmt.Println(i, string(s))
+					}
+
+				}
+			}
+
+		}
 	}
 
 }
 
-// TO-DO: fix this just like read_file
 // this function might be useless, don't think daemon is going to
 // receive a message like this... :(
 func read_hardwarepool(msg *message.Message) {
-	msgHardware := msg.GetHardwarePool(new(message.GetHardwarePool))
 
-	// TO-DO: use hardware info idk XD
-	//fmt.Println(string(msgHardware.Hardware()))
-	// print everyting in the hardware
-	for i := 0; i < msgHardware.HardwareLength(); i++ {
-		fmt.Println(string(msgHardware.Hardware(i)))
+	fmt.Println("reading hardware...")
+
+	unionTable := new(flatbuffers.Table)
+
+	if msg.Body(unionTable) {
+
+		unionType := msg.BodyType()
+
+		if unionType == message.MessageBodyGetHardwarePool {
+			unionHardware := new(message.GetHardwarePool)
+			unionHardware.Init(unionTable.Bytes, unionTable.Pos)
+
+			// TO-DO: what are we going to do with the unionHardware?
+			// do something here
+		}
 	}
 }
 
-// TO-DO: fix this just like read_file
 func read_result(msg *message.Message) {
-	msgResult := msg.GetResult(new(message.GetResult))
 
-	// print everyting in the result
-	for i := 0; i < msgResult.IdListLength(); i++ {
-		fmt.Println(string(msgResult.IdList(i)))
+	fmt.Println("reading result...")
+
+	unionTable := new(flatbuffers.Table)
+
+	if msg.Body(unionTable) {
+
+		unionType := msg.BodyType()
+
+		if unionType == message.MessageBodyGetResult {
+			unionResult := new(message.GetResult)
+			unionResult.Init(unionTable.Bytes, unionTable.Pos)
+
+			// TO-DO: what are we going to do with the unionResult?
+			// do something here
+		}
 	}
+
+	/*
+		msgResult := msg.GetResult(new(message.GetResult))
+
+		// print everyting in the result
+		for i := 0; i < msgResult.IdListLength(); i++ {
+			fmt.Println(string(msgResult.IdList(i)))
+		}*/
 }
 
 func read_file(msg *message.Message) {
+
+	fmt.Println("reading file...")
+
 	unionTable := new(flatbuffers.Table)
 
 	if msg.Body(unionTable) {
@@ -241,6 +243,28 @@ func read_file(msg *message.Message) {
 
 }
 
+// returns an open file or opens new file instead
+func getOutputFile(filename string) *os.File {
+
+	output_file, opened := open_files[filename]
+
+	if !opened {
+		output_file, err := os.OpenFile(string(filename), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+
+		if err != nil {
+			log.Fatal(err)
+
+			// unused variable fix
+			fmt.Println(opened, output_file)
+		} else {
+			// save the opened file to the map
+			open_files[filename] = output_file
+		}
+	}
+
+	return output_file
+}
+
 func saveFile(msgFile *message.File) {
 	filename := msgFile.Filename()
 	packetnr := msgFile.Packetnumber()
@@ -252,33 +276,17 @@ func saveFile(msgFile *message.File) {
 	arr := msgFile.DataBytes()
 
 	// save arr to output file
-	output_file, err := os.OpenFile(string(filename), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	output_file := getOutputFile(string(filename))
+	/*output_file, err := os.OpenFile(string(filename), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 
 	if err != nil {
 		log.Fatal(err)
-	}
+	}*/
 
 	output_file.Write(arr)
 
 	output_file.Close()
 }
-
-// func write_message(msg string) []byte {
-
-// 	builder := flatbuffers.NewBuilder(1024)
-// 	hello := builder.CreateString(msg)
-// 	hi := builder.CreateString("")
-
-// 	message.MessageStart(builder)
-// 	message.MessageAddAgentId(builder, hi)
-// 	message.MessageAddCmd(builder, hello)
-// 	binMsg := message.MessageEnd(builder)
-// 	builder.Finish(binMsg)
-// 	buf := builder.FinishedBytes()
-// 	return buf
-// }
-
-// WIP
 
 // WIP
 func run_daemon() {
